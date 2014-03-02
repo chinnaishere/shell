cdb2e04eba954465790f2d3b900d286489329782
Nikolay Feldman <nf139@cs.rutgers.edu>
2014-03-02 17:20:36 -0500

    conflict with comments, pushed mine, includes malloc free

--- shell.c
+++ shell.c
@@ -14,8 +14,8 @@ typedef struct Token {
 } Token;
 
 typedef struct Command {
-	int cmdargc; //number of arguments
-	char **cmdargv; //args to pass to that call
+	int cmdargc; /*number of arguments*/
+	char **cmdargv; /*args to pass to that call*/
 	int fd[2];
 } Command;
 
@@ -39,11 +39,11 @@ void clean();
 
 int parse(char *buffer, int buflen, Token *tokens, int *tokensSize, int argc, char **argv){
 	int i, dQuoteOpen=0, sQuoteOpen=0;
-	int start = -1; // start of token, end of token will be i, -1 means need a starting point
+	int start = -1; /* start of token, end of token will be i, -1 means need a starting point*/
 	for (i = 0; i < buflen; ++i) 
 	{
 		if (buffer[i] == '"' && !sQuoteOpen) {
-			if (dQuoteOpen) { //closing double quote found, make token
+			if (dQuoteOpen) { /*closing double quote found, make token*/
 				tokens[*tokensSize].start = start;
 				tokens[*tokensSize].len = i - start;
 				++(*tokensSize);
@@ -51,10 +51,10 @@ int parse(char *buffer, int buflen, Token *tokens, int *tokensSize, int argc, ch
 				start = -1;
 			} else {
 				dQuoteOpen = 1;
-				start = ++i; // assuming there won't be 2 double quotes back to back, i = '"', i+1=next char
+				start = ++i; /* assuming there won't be 2 double quotes back to back, i = '"', i+1=next char*/
 			} 
 		} else if (buffer[i] == '\'' && !dQuoteOpen) {
-			if (sQuoteOpen) { //closing singe quote found, make token
+			if (sQuoteOpen) { /*closing singe quote found, make token*/
 				tokens[*tokensSize].start = start;
 				tokens[*tokensSize].len = i - start;
 				++(*tokensSize);
@@ -62,37 +62,37 @@ int parse(char *buffer, int buflen, Token *tokens, int *tokensSize, int argc, ch
 				start = -1;
 			} else {
 				sQuoteOpen = 1;
-				start = ++i; // assuming there won't be 2 double quotes back to back, i = '"', i+1=next char
+				start = ++i; /* assuming there won't be 2 double quotes back to back, i = '"', i+1=next char*/
 			}
-		} else { //any character or space
+		} else { /*any character or space*/
 			if (sQuoteOpen || dQuoteOpen) 
 				continue;
 			if (start == -1) {
-				if (buffer[i] == ' ') //space, continue onto next char, still looking for start
+				if (buffer[i] == ' ') /*space, continue onto next char, still looking for start*/
 					continue;
-				start = i; //nonspace, now our starting point
+				start = i; /*nonspace, now our starting point*/
 				if (buffer[i] == '|') {
 					tokens[*tokensSize].start = start;
 					tokens[*tokensSize].len = 1;
 					++(*tokensSize);
 					start = -1;
 				}
-			} else { //not looking for start, token is already building
-				if (buffer[i] == ' ' || buffer[i] == '|') { //space or pipe found, token completed
+			} else { /*not looking for start, token is already building*/
+				if (buffer[i] == ' ' || buffer[i] == '|') { /*space or pipe found, token completed*/
 					tokens[*tokensSize].start = start;
 					tokens[*tokensSize].len = i - start;
 					++(*tokensSize);
 					start = -1;
-					if (buffer[i] == '|') //dec i, loop incs and reruns on pipe. (less code)
+					if (buffer[i] == '|') /*dec i, loop incs and reruns on pipe. (less code)*/
 						--i;
 				}
 			}
 		}
 	}
 
-	if (start > -1) { //loop ended with last token still not added to array
+	if (start > -1) { /*loop ended with last token still not added to array*/
 		if (dQuoteOpen || sQuoteOpen)
-			return 1; //error
+			return 1; /*error*/
 		tokens[*tokensSize].start = start;
 		tokens[*tokensSize].len = i - start;
 		++(*tokensSize);
@@ -116,12 +116,12 @@ void tokensToCommands(char *buffer, Token *tokens, int tokensSize){
 	int start = 0;
 	int i;
 	for (i = 0; i < tokensSize; ++i) {
-		// i will hold the index of a pipe token
+		/* i will hold the index of a pipe token*/
 		if (tokens[i].len == 1 && buffer[tokens[i].start] == '|') {
 			int j, a, argsize = i-start;
 			cmds[cmdsSize].cmdargv = (char **)malloc(sizeof(char *) * (argsize+1));
 			cmds[cmdsSize].cmdargv[argsize] = 0;
-			for (j = start, a = 0; j < i; ++j, ++a) { //go through all tokens up to the pipe which is in cell i
+			for (j = start, a = 0; j < i; ++j, ++a) { /*go through all tokens up to the pipe which is in cell i*/
 				char *tok = (char *)malloc(sizeof(char)*(tokens[j].len + 1));
 				tok[0] = '\0';
 				strncat(tok, buffer + tokens[j].start, tokens[j].len);
@@ -134,11 +134,11 @@ void tokensToCommands(char *buffer, Token *tokens, int tokensSize){
 		}
 	}
 
-	//last command, not going  to be "commandized" in loop
+	/*last command, not going  to be "commandized" in loop*/
 	int j, a, argsize = i-start;
 	cmds[cmdsSize].cmdargv = (char **)malloc(sizeof(char *) * (argsize+1));
 	cmds[cmdsSize].cmdargv[argsize] = 0;
-	for (j = start, a = 0; j < i; ++j, ++a) { //go through all tokens up to the pipe which is in cell i
+	for (j = start, a = 0; j < i; ++j, ++a) { /*go through all tokens up to the pipe which is in cell i*/
 		char *tok = (char *)malloc(sizeof(char)*(tokens[j].len + 1));
 		tok[0] = '\0';
 		strncat(tok, buffer + tokens[j].start, tokens[j].len);
@@ -151,16 +151,16 @@ void tokensToCommands(char *buffer, Token *tokens, int tokensSize){
 
 int main(int argc, char **argv){
 
-	//create builtins
+	/*create builtins*/
 	struct Builtins builtins[2];
 	builtins[0].name = "cd";
 	builtins[0].f = &shell_cd;
 	builtins[1].name = "exit";
 	builtins[1].f = &shell_exit;
 
-	//create tokens array
+	/*create tokens array*/
 	Token tokens[50];
-	int tokensSize = 0; //Number of elements in Token array
+	int tokensSize = 0; /*Number of elements in Token array*/
 
 	char buffer[BUFSIZE];
 	int pid, status;
@@ -188,7 +188,7 @@ int main(int argc, char **argv){
 		}
 		printf("command size: %d\n",cmdsSize);
 
-		//when there is only one command, no pipes are needed
+		/*when there is only one command, no pipes are needed*/
 		if(cmdsSize == 1){
 			if (checkBuiltins(&cmds[0], builtins)) {
 				clean();
@@ -198,30 +198,30 @@ int main(int argc, char **argv){
 			}
 			runonecmd(&cmds[0]);
 		}else{
-			//show when you try any commands that have at least one pipe.
+			/*show when you try any commands that have at least one pipe.*/
 			char** allcommands[cmdsSize];
 			for (i = 0; i < cmdsSize; ++i)
 				allcommands[i] = cmds[i].cmdargv;
 
 
 			int currcmd = 0;
-			//runs the amount of commands inputted
+			/*runs the amount of commands inputted*/
 			while(currcmd < cmdsSize){
 
-				//for the first command only one end of the pipe has to be closed
+				/*for the first command only one end of the pipe has to be closed*/
 				if(currcmd == 0){
 					pipe(cmds[currcmd].fd);
 					runcmd(-1, cmds[currcmd].fd[1], allcommands[currcmd]);
 					close(cmds[currcmd].fd[1]);
 				}else if(currcmd == (cmdsSize-1)){
-					//for the last command the last pipe end has to be closed as well as printing out the process status
+					/*for the last command the last pipe end has to be closed as well as printing out the process status*/
 					runcmd(cmds[currcmd-1].fd[0], -1, allcommands[currcmd]);
 					close(cmds[currcmd-1].fd[0]);
 
-					//this is the last command
+					/*this is the last command*/
 					break;
 				}else{
-					//for all commands in the middle the std in and out of the pipes must be closed
+					/*for all commands in the middle the std in and out of the pipes must be closed*/
 					pipe(cmds[currcmd].fd);
 					runcmd(cmds[currcmd-1].fd[0], cmds[currcmd].fd[1], allcommands[currcmd]);
 					close(cmds[currcmd-1].fd[0]); 
@@ -274,17 +274,17 @@ void runcmd(int in, int out, char **cmd){
 	 */
 void runonecmd(Command * cmd){
 	int pid;
-	// char* temp[] = {cmd[0].cmd, cmd[0].args, NULL};
+	/* char* temp[] = {cmd[0].cmd, cmd[0].args, NULL};*/
 	switch (pid = fork()) {
 
 		case 0: 
-			//child
+			/*child*/
 			fprintf(stderr, "execvp(\"%s\")\n", cmd->cmdargv[0]);
-			execvp(cmd->cmdargv[0], cmd->cmdargv);  //run command
-			perror(cmd->cmdargv[0]);    //something went wrong!
+			execvp(cmd->cmdargv[0], cmd->cmdargv);  /*run command*/
+			perror(cmd->cmdargv[0]);    /*something went wrong!*/
 
 		default: 
-			//parent does nothing
+			/*parent does nothing*/
 			break;
 
 		case -1:
@@ -295,12 +295,12 @@ void runonecmd(Command * cmd){
 
 void shell_cd(Command *cmd)
 {
-	if (cmd->cmdargc == 1) { //change to home dir
+	if (cmd->cmdargc == 1) { /*change to home dir*/
 		if (chdir(getenv("HOME")) == -1) {
 			perror("ERROR: Failed to change to HOME dir.");
 			return;
 		}
-	} else if (cmd->cmdargc == 2) { //change to path
+	} else if (cmd->cmdargc == 2) { /*change to path*/
 		if (chdir(cmd->cmdargv[1]) == -1) {
 			perror("ERROR: Failed to change dir.");
 			return;
@@ -309,7 +309,7 @@ void shell_cd(Command *cmd)
 		fprintf(stderr, "ERROR:Invalid number of arguments for cd\n");
 		return;
 	}
-	//print current working dir
+	/*print current working dir*/
 	char *dir = getcwd(NULL, 200);
 	printf("\n%s", dir);
 	free(dir);
